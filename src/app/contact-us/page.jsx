b1028916ff6091463151976d4f6303f129beeee5
Pranav <mpranavprem@gmail.com>
2024-08-16 21:19:12 +0530

    styles changed in contact us

--- src/app/contact-us/page.jsx
+++ src/app/contact-us/page.jsx
@@ -9,7 +9,7 @@ const ContactUs = () => {
   return (
     <div className="wrapper grow relative">
       <div className="flex flex-col gap-8 my-10">
-        <div className="flex flex-col gap-3">
+        <div className="flex flex-col gap-6">
           <h2 className="text-3xl sm:text-4xl lg:text-5xl font-extrabold leading-tight">
             Get in Touch with <br />
             <span className="text-white">Goal</span>
@@ -22,31 +22,36 @@ const ContactUs = () => {
             connect!
           </p>
           <h5 className="font-semibold text-lg">How can we help?</h5>
-          <p>
-            <span className="font-semibold text-lg">&bull; General Inquiries: </span>
-            {
-              "Got a question about our content or want to know more about GoalNet? We’re here to assist you."
-            }
-          </p>
-          <p>
-            <span className="font-semibold text-lg">&bull; Feedback: </span>
-            {
-              "Your thoughts help us improve! Share your opinions on our articles, features, or anything else you’d like to see on GoalNet."
-            }
-          </p>
-          <p>
-            <span className="font-semibold text-lg">&bull; Collaborations: </span>
-            {
-              "Interested in working with us? Whether it's a partnership or a feature request, we’re open to collaborating with like-minded football enthusiasts."
-            }
-          </p>
+          <div className="flex flex-col gap-3">
+            <p>
+              <span className="font-semibold">
+                &bull; General Inquiries:{" "}
+              </span>
+              {
+                "Got a question about our content or want to know more about GoalNet? We’re here to assist you."
+              }
+            </p>
+            <p>
+              <span className="font-semibold">&bull; Feedback: </span>
+              {
+                "Your thoughts help us improve! Share your opinions on our articles, features, or anything else you’d like to see on GoalNet."
+              }
+            </p>
+            <p>
+              <span className="font-semibold">
+                &bull; Collaborations:{" "}
+              </span>
+              {
+                "Interested in working with us? Whether it's a partnership or a feature request, we’re open to collaborating with like-minded football enthusiasts."
+              }
+            </p>
+          </div>
           <h5 className="font-semibold text-lg">How to Reach Us:</h5>
           <p>
             Simply click the button below to send us an email. We’ll do our best
             to get back to you as quickly as possible. Let’s keep the football
             conversation going and make GoalNet even better together!
           </p>
-          <h5 className="font-semibold text-lg">[Email Us Now]</h5>
         </div>
         <Image
           src="/assets/images/logo.png"
@@ -55,7 +60,14 @@ const ContactUs = () => {
           height={500}
           className="blur-sm absolute -rotate-12 inset-0 w-full h-full object-contain sm:object-cover -z-10 brightness-[20%]"
         />
-        <Link href="mailto:contactgoalnet@gmail.com" rel="noopener noreferrer" target="_blank" className="primary-btn text-center h-fit w-[15rem]">Contact Us</Link>
+        <Link
+          href="mailto:contactgoalnet@gmail.com"
+          rel="noopener noreferrer"
+          target="_blank"
+          className="primary-btn text-center h-fit w-[15rem]"
+        >
+          Email Us Now
+        </Link>
       </div>
     </div>
   );
