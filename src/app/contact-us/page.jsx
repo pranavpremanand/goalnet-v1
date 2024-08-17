ea6685af84ee4e8a6e0e7643628f4d78a6638058
Pranav <mpranavprem@gmail.com>
2024-08-18 00:30:25 +0530

    contact us style changes

--- src/app/contact-us/page.jsx
+++ src/app/contact-us/page.jsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata = {
   title: "Contact Us",
@@ -9,13 +15,13 @@ const ContactUs = () => {
   return (
     <div className="wrapper grow relative">
       <div className="flex flex-col gap-8 my-10">
-        <div className="flex flex-col gap-6">
+        <div className="flex flex-col gap-6 max-w-4xl">
           <div className="text-2xl sm:text-3xl lg:text-4xl font-extrabold">
             Get in Touch with <br />
-            <span className="text-3xl sm:text-4xl lg:text-5xl">
+            <h1 className={`${poppins.className} text-3xl sm:text-4xl lg:text-5xl mt-3`}>
               <span className="text-white">Goal</span>
               <span className="text-primary">Net.</span>
-            </span>
+            </h1>
           </div>
           <p className="text-gray-300">
             At GoalNet, we value your input and are always eager to hear from
