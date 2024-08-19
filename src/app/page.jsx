3113ba0215e56e47acef147f7e187c1325d29093
Pranav Premanand <107985394+pranavpremanand@users.noreply.github.com>
2024-08-19 18:33:08 +0530

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,5 @@
 import Banner from "../components/Banner";
 import PostsList from "@/components/PostsList";
-// import LatestTransfers from "@/components/LatestTransfers";
 
 export const metadata = {
   title: "Home",
@@ -10,7 +9,6 @@ export default function Home() {
   return (
     <section className="w-full grow">
       <Banner />
-      // <LatestTransfers />
       <PostsList />
     </section>
   );
